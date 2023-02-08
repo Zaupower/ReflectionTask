a59f9737c32e3ce74711e2ed83d0d1b686773687
Zaupower <56695737+Zaupower@users.noreply.github.com>
2023-02-08 20:58:13 +0000

    Eliminate comments

--- ReflectionTaskLibrary/CustomConverter.cs
+++ ReflectionTaskLibrary/CustomConverter.cs
@@ -12,55 +12,7 @@ namespace ReflectionTaskLibrary
         private int spacer = -1;
         public string Serialize(object model)
         {
-            //IncrementSpacer();
-
-            //StringBuilder sb = new StringBuilder();
-            //sb.Append(GetSpacer() + "[section.begin]");
-            //sb.AppendLine();
-            //// Get all properties in the model
-            //var properties  = model.GetType().GetProperties();
-
-            //foreach (var property in properties)
-            //{
-            //    // Check if property has the CustomSerializeAttribute attribute
-            //    //var customSerializeAttributes = property.GetCustomAttributes(typeof(CustomSerializeAttribute), true);
-            //    var customSerializeAttributes = property.GetCustomAttributes().Select(i=> (CustomSerializeAttribute)i).ToList();
-            //    if (customSerializeAttributes.Count > 0)
-            //    {
-            //        // Get the Name attribute parameter value
-            //        string name = customSerializeAttributes[0].Name;
-            //        object value = property.GetValue(model);
-
-            //        if (property.PropertyType.IsClass && property.PropertyType != typeof(string))
-            //        {
-            //            value = null;
-            //        }                        
-
-            //        if (string.IsNullOrEmpty(name))
-            //        {
-            //            name = property.Name;
-            //        }                  
-
-            //        // Serialize the property value
-            //        sb.AppendFormat(GetSpacer()+"{0} = {1}", name, value);
-            //        sb.AppendLine();
-            //        // If the property value is a complex entity with nested fields, serialize them recursively
-            //        if (property.PropertyType.IsClass && property.PropertyType != typeof(string))
-            //        {
-            //            sb.Append(Serialize(property.GetValue(model)));
-            //        }
-            //    }
-            //}
-            //sb.Append(GetSpacer() + "[section.end]");
-            //sb.AppendLine();
-            //DecrementSpacer();
             string result = "";
-            //switch (model.GetType())
-            //{
-            //    case :
-            //        result = SerializeString(model);
-            //        break;
-            //}
             if(model.GetType() == typeof(string)
                 || model.GetType() == typeof(DayOfWeek)
                 || model.GetType() == typeof(int)
@@ -81,9 +33,6 @@ namespace ReflectionTaskLibrary
             return model.ToString();
         }
 
-        //TODO:
-        //Handle null values; If the propertie does not have value(null) dont add it
-        //
         public string Serialize2(object model)
         {
             IncrementSpacer();
@@ -125,19 +74,6 @@ namespace ReflectionTaskLibrary
             DecrementSpacer();
             return sb.ToString();
         }
-
-            private StringBuilder MethodBuidler(StringBuilder sb, PropertyInfo[] properties)
-        {
-            foreach (var property in properties)
-            {
-                sb.Append("[section.begin]\n");
-                sb.Append(property.Name + " = " + properties.GetValue(0).ToString());
-                sb.Append("[section.end]\n");
-            }
-
-            return sb;
-        }
-
 
         public string GetSpacer()
         {
